36d83c4b5abb619029e94d6ca32a714c3bc5a17a
PiyushDaga2007 <piyushdaga27@yahoo.com>
2021-07-22 16:56:33 +0530

    Hello

--- sketch.js
+++ sketch.js
@@ -7,6 +7,8 @@ var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
 var bird, slingShot;
+var gameState="onSling";
+
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -70,10 +72,19 @@ function draw(){
 }
 
 function mouseDragged(){
+    if(gameState!="launched"){
     Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+gameState="launched"
+}
+function keyPressed(){
+    if(keyCode===32){
+        slingshot.attach(bird.body);
+    }
+    
 }
